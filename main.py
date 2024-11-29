7458f56cd0c156197a05171e6c5fb7d5693df2b1
Kakalasek <example@email.com>
2024-11-30 00:06:08 +0100

    extracting content

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ if __name__ == "__main__":
                 'content': None, 
                 'date': None}
 
+    # Extract links
+    a_tags = soup.find_all('a', href=True)
+
+    links = filter(lambda link: "/zpravy" in link['href'], a_tags)
+    links = [link['href'] for link in links]
+    links = set(links)
+    links = list(links)
+
     # Title
     title = soup.find('title').contents[0]
     json_out['title'] = title
@@ -22,6 +30,23 @@ if __name__ == "__main__":
     categories = soup.find('meta', attrs={'name': 'keywords'})['content']
     json_out['categories'] = categories.replace(' ', '').split(',')
 
+    # Images
+    images = soup.find_all('img')
+    images = [img['src'] for img in images]
+    json_out['number_of_images'] = len(images)
+
+    # Content
+    article_content = soup.find('div', id='articlebody')
+    article_content = soup.find_all('p', attrs={'class': None}, id=None)
+    article_content = filter(lambda p: not p.find('span'), article_content)
+    article_content = [p.text for p in article_content]
+    out = ""
+    for content in article_content:
+        out += (content + " ")
+
+    json_out['content'] = out
+
+    
 
     # Date
     date = soup.find('span', itemprop='datePublished')['content']
