60228fdff59b8c776a44f6e822ea9a0006e40c8f
Kakalasek <example@email.com>
2024-12-03 10:05:21 +0100

    little rebuilld

--- main.py
+++ main.py
@@ -1,36 +1,59 @@
-import requests
 from bs4 import BeautifulSoup
-import json
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 import time
+import json
 
-if __name__ == "__main__":
-    driver = webdriver.Firefox()
-    current_url = "https://www.ceskenoviny.cz/zpravy/afp-trosky-z-notre-dame-jsou-uschovane-a-studovane-na-tajnem-miste/2601030" 
-    traversed_links = []
-    untraversed_links = []
+
+def Crawler(driver: webdriver, start_url: str, parser: str, backup_urls: list):
+
+    current_url = start_url
     scroll_count = 5
-    json_data = {"json_data": []}
-    json_object = json.dumps(json_data)
-    with open('output.json', 'w') as file:
-        file.write(json_object)
+    traversed_links = set()
+    untraversed_links = set()
 
-    while True:
+    def json_init():
+        json_data = {"json_data": []}
+        json_object = json.dumps(json_data)
+        with open('output.json', 'w') as file:
+            file.write(json_object)
+
+    def dump_to_file(json_data):
+
+        with open('output.json', 'r+') as file:
+
+            file_data = json.load(file)
+
+            file_data['json_data'].append(json_data)
+
+            file.seek(0)
+
+            json.dump(file_data, file, indent = 4)
+
+
+    def set_next_url():
+        global current_url
+
         driver.get(current_url)
         for _ in range(scroll_count):
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(2)
         page_source = driver.page_source
-        traversed_links.append(current_url)
-        soup = BeautifulSoup(page_source, 'lxml')
+        traversed_links.add(current_url)
+
+        return page_source
+    
+    
+    def get_data(page_to_crawl):
+        global current_url
+
+        soup = BeautifulSoup(page_to_crawl, 'lxml')
         json_out = {'title': None, 
                     'categories': None, 
                     'number_of_images': None, 
                     'content': None, 
                     'date': None}
-
-        # Extract links
+        
         a_tags = soup.find_all('a', href=True)
 
         links = filter(lambda link: "/zpravy" in link['href'], a_tags)
@@ -41,27 +64,23 @@ if __name__ == "__main__":
 
         for link in links:
             if link not in traversed_links:
-                untraversed_links.append(link)
+                untraversed_links.add(link)
 
         while True:
             current_url = untraversed_links.pop()
             if current_url not in traversed_links:
                 break
 
-        # Title
         title = soup.find('title').contents[0]
         json_out['title'] = title
 
-        # Categories
         categories = soup.find('meta', attrs={'name': 'keywords'})['content']
         json_out['categories'] = categories.replace(' ', '').split(',')
 
-        # Images
         images = soup.find_all('img')
         images = [img['src'] for img in images]
         json_out['number_of_images'] = len(images)
 
-        # Content
         article_content = soup.find('div', id='articlebody')
         article_content = soup.find_all('p', attrs={'class': None}, id=None)
         article_content = filter(lambda p: not p.find('span'), article_content)
@@ -72,17 +91,43 @@ if __name__ == "__main__":
 
         json_out['content'] = out
 
-        # Date
         date = soup.find('span', itemprop='datePublished')['content']
         json_out['date'] = date
 
-        # Dump to JSON
-        with open('output.json', 'r+') as file:
+        return json_out
 
-            file_data = json.load(file)
+    def crawl():
+        global current_url
 
-            file_data['json_data'].append(json_out)
+        try:
+            while True:
+                page_to_crawl = set_next_url()
+                json_data = get_data(page_to_crawl)
+                dump_to_file(json_data)
 
-            file.seek(0)
 
-            json.dump(file_data, file, indent = 4)
+
+        except Exception:
+            if len(backup_urls) <= 0:
+                raise LookupError("Everything crawled")
+            current_url = backup_urls.pop()
+            crawl()
+        
+
+    return {'json_init': json_init,'crawl': crawl}
+
+
+if __name__ == "__main__":
+
+    crawler = Crawler(driver=webdriver.Firefox(), 
+                    parser='lxml', 
+                    start_url="https://www.ceskenoviny.cz/zpravy/afp-trosky-z-notre-dame-jsou-uschovane-a-studovane-na-tajnem-miste/2601030", 
+                    backup_urls=["https://www.ceskenoviny.cz/zpravy/satelitni-mytny-system-za-pet-let-fungovani-vybral-722-miliardy-kc/2603629",
+                                "https://www.ceskenoviny.cz/zpravy/na-znovuotevreni-notre-dame-dorazi-i-trump-v-akci-bude-6000-policistu/2603610",
+                                "https://www.ceskenoviny.cz/zpravy/fotbalistky-se-v-odvete-baraze-utkaji-s-portugalskem-o-historicky-postup-na-euro/2603331",
+                                "https://www.ceskenoviny.cz/zpravy/studie-vitamin-e-v-elektronickych-cigaretach-poskozuje-ochrannou-vrstvu-plic/2603337",
+                                "https://www.ceskenoviny.cz/zpravy/zive-firma-compas-expert-na-prumyslovou-automatizaci-se-predstavi-na-msv-2024/2579886"])
+
+
+    crawler['json_init']()
+    crawler['crawl']()
